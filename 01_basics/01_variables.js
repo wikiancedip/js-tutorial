550c9890a099cd962dcfa4d344a8b17899e03a94
Debarghya Das <dasdebarghya15@gmail.com>
2024-04-02 01:37:35 +0530

    add new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 798069
+let accountEmail = "debraya.googlw@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+console.table([accountEmail,accountId,accountPassword,])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all JS code as newer version
+
+
+// Primitive Datatypes
+
+// number
+// bigInt
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+//object
