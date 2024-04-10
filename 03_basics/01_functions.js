a1eb8ab24c93da8a19067a2627d8d88210b1e0c1
Debarghya Das <dasdebarghya15@gmail.com>
2024-04-11 02:16:50 +0530

    adding functions in javascript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// Functions in JavaScript
+
+function sayMyName() {
+    console.log("D");
+    console.log("E");
+    console.log("B");
+    console.log("A");
+    console.log("R");
+    console.log("G");
+    console.log("H");
+    console.log("Y");
+    console.log("A");
+}
+
+sayMyName()
+
