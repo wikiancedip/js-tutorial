7e4013c4db1f182108087915579935960a18ddd4
Debarghya Das <dasdebarghya15@gmail.com>
2024-05-27 22:23:53 +0530

    added new functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,5 +12,36 @@ function sayMyName() {
     console.log("A");
 }
 
-sayMyName()
+// sayMyName() //Execution
+
+// function addTwoNumbers(number1, number2) {
+
+//     console.log(number1 + number2);
+
+// }
+
+function addTwoNumbers(number1, number2) {
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+
+
+}
+
+const result = addTwoNumbers(1, 2)
+
+console.log("Result: ", result);
+
+
+function loginUserMessage(username = "Sayan"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Debarghya"))
+
 
